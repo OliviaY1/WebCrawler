e592b00d8eaa5acabac33e4c19096aeea6b6f89a
Xi Yan <yanqianyanxi@outlook.com>
2023-03-04 07:53:23 -0500

    Download a Google Search Page

--- GoogleSearchEngine/main.py
+++ GoogleSearchEngine/main.py
@@ -5,10 +5,19 @@ The html file would be downloaded on the same catalogue -- by Xi Yan'''
 headers = {
     'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36'
 }
-kw = input('Enter what you want to search on Google: ')
-param = {'q': kw}
-response = requests.get(url='https://www.google.ca/search', params=param, headers=headers)
-filename = './GoogleSearchEngine/download_pages/'+str(kw)+'.html'
-with open(filename, 'w', encoding='utf-8') as f:
-    f.write(response.text)
-print('Successfully downloaded the html file')
+exit_ = False
+while not exit_:
+    kw = input('Enter what you want to search on Google: ')
+    param = {'q': kw}
+    response = requests.get(url='https://www.google.ca/search', params=param, headers=headers)
+    filename = './GoogleSearchEngine/download_pages/'+str(kw)+'.html'
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.write(response.text)
+    print('Successfully downloaded the html file')
+    ans = input('Do you wanna continue searching? (Y/N) ')
+    while (ans != 'Y') and (ans != 'N'):
+        ans = input("Please enter 'Y' or 'N'\nDo you wanna continue searching? (Y/N) ")
+    if ans == 'Y':
+        continue
+    else:
+        exit_ = True
